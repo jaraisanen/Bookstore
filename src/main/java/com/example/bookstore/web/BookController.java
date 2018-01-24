1e0ba15c8904d51df5c4c9abda85b1d27244269c
Jarno <a1602746@myy.haaga-helia.fi>
2018-01-24 15:52:57 +0200

    REST API provided for books

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,23 +1,34 @@
-package com.example.bookstore.controller;
+package com.example.bookstore.web;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.ManyToOne;
+import java.util.List;
 
 @Controller
 public class BookController {
     @Autowired
+    @ManyToOne
+    @JsonIgnore
     private BookRepository repository;
-    @RequestMapping(value="/")
+
+    @RequestMapping(value = "/")
+
     public String index() {
         return "index";
     }
+
     @RequestMapping("/booklist")
+
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
@@ -35,10 +46,27 @@ public class BookController {
         return "redirect:../booklist";
     }
 
-    @RequestMapping(value="/save", method = RequestMethod.POST)
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveBook(Book book) {
         repository.save(book);
         return "redirect:booklist";
     }
 
+    // Rest Api provider methods
+
+    @RequestMapping(value = "/api", method = RequestMethod.GET)
+    public @ResponseBody
+    List<Book> bookListRest() {
+        return (List<Book>) repository.findAll();
+    }
+
+    @RequestMapping(value = "/api/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Book findBookRest(@PathVariable("id") Long bookId) {
+        return repository.findOne(bookId);
+    }
 }
+
+
+
+
